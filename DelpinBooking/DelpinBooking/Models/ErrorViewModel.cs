cc47943b172951b949d29ecfa274d6db7f5c5833
majaknielsen <83395792+majaknielsen@users.noreply.github.com>
2021-04-29 10:42:29 +0200

    delpin

---
+++ DelpinBooking/DelpinBooking/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DelpinBooking.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
