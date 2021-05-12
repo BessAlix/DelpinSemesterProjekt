c4895f2d194d3f0b02e30040a799f4bc5ee7196c
DirtyGurd <sigurdbk@gmail.com>
2021-05-12 17:13:35 +0200

    Drop down stuff

--- DelpinBooking/DelpinBooking/Data/Migrations/20210512150355_RealRegisterUpdate.Designer.cs
+++ DelpinBooking/DelpinBooking/Data/Migrations/20210512150355_RealRegisterUpdate.Designer.cs
@@ -4,14 +4,16 @@ using DelpinBooking.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DelpinBooking.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210512150355_RealRegisterUpdate")]
+    partial class RealRegisterUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
