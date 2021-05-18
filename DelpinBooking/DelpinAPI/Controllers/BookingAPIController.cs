86bdff8496c5f3039cc54987fd3021b1bf5c7a85
majaknielsen <83395792+majaknielsen@users.noreply.github.com>
2021-05-18 10:03:32 +0200

    GetBookingsForCustomer metode i API

--- DelpinBooking/DelpinAPI/Controllers/BookingAPIController.cs
+++ DelpinBooking/DelpinAPI/Controllers/BookingAPIController.cs
@@ -1,4 +1,5 @@
-﻿using DelpinAPI.Data;
+﻿using System.Linq;
+using DelpinAPI.Data;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
@@ -29,12 +30,12 @@ namespace DelpinAPI.Controllers
         }
 
         [HttpGet]
-        [Route("[action]/{id}")]
-        public async Task<IActionResult> GetBooking(int id)
+        [Route("[action]/{bookingId}")]
+        public async Task<IActionResult> GetBooking(int bookingId)
         {
             var booking = await _context.Booking
                 .AsNoTracking()
-                .FirstOrDefaultAsync(b => b.Id == id);
+                .FirstOrDefaultAsync(b => b.Id == bookingId);
 
             return Ok(booking);
         }
@@ -49,5 +50,16 @@ namespace DelpinAPI.Controllers
             return CreatedAtAction("GetBooking", new { id = booking.Id }, booking);
         }
 
+        [HttpGet]
+        [Route("[action]/{customerId}")]
+        public async Task<IActionResult> GetBookingsForCustomer(string customerId)
+        {
+            var bookings = await _context.Booking
+                .AsNoTracking()
+                .Where(b => b.Customer == customerId)
+                .ToListAsync();
+
+            return Ok(bookings);
+        }
     }
 }
